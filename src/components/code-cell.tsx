fa738dd4a329cae23f6569cd57872a51ad5ea385
ArkadiuszBalakier <arkadiusz.balakier@gmail.com>
2023-05-11 14:37:52 +0200

    code-cell.css

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -5,6 +5,7 @@ import Resizable from "./resizable";
 import { Cell } from "../state";
 import { useActions } from "../hooks/use-actions";
 import { useTypedSelector } from "../hooks/use-type-selector";
+import "./code-cell.css";
 
 interface CodeCellProps {
   cell: Cell;
@@ -45,11 +46,17 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
             initialValue={cell.content}
           />
         </Resizable>
-        {!bundle || bundle.loading ? (
-          <div> Loading ... </div>
-        ) : (
-          <Preview code={bundle.code} bundlingStatus={bundle.err} />
-        )}
+        <div className="progress-wrapper">
+          {!bundle || bundle.loading ? (
+            <div className="progress-cover">
+              <progress className="progress is-small is-primary" max="100">
+                Loading
+              </progress>
+            </div>
+          ) : (
+            <Preview code={bundle.code} bundlingStatus={bundle.err} />
+          )}
+        </div>
       </div>
     </Resizable>
   );
